127b933ae883e8533bbd3f7ebe53e77e72d3bfff
Joe Awwad <joe.awwad@getray.com>
2020-06-05 15:07:42 +0300

    Initial Project Add

---
+++ src/main/java/leo/spring5webapp/drawmeahero/DrawmeaheroApplication.java
@@ -0,0 +1,13 @@
+package leo.spring5webapp.drawmeahero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawmeaheroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawmeaheroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/leo/spring5webapp/drawmeahero/DrawmeaheroApplicationTests.java
@@ -0,0 +1,13 @@
+package leo.spring5webapp.drawmeahero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawmeaheroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
