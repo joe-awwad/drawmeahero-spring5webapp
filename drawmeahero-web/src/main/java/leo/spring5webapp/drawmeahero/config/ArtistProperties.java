690747677c5b193c8242e7db815e7c10b651b5bb
Joe Awwad <joe.awwad@getray.com>
2020-06-11 16:32:45 +0300

    Adds artist configuration properties. Closes #15

---
+++ drawmeahero-web/src/main/java/leo/spring5webapp/drawmeahero/config/ArtistProperties.java
@@ -0,0 +1,48 @@
+package leo.spring5webapp.drawmeahero.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("dmah.artist")
+public class ArtistProperties {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
